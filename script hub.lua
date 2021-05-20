317b9a7ab39a1b4cfc9f135c779b278fb1e78b99
yeah <61770151+zopac@users.noreply.github.com>
2021-05-19 20:46:26 -0500

    the

--- script hub.lua
+++ script hub.lua
@@ -1,8 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zopac/lua-stuff/main/.lua", true))()
 
--- Bruh Hub: Phantom Forces, Shindo Life 2, Vesteria, Ragdoll Engine, Ro-Ghoul, Project: Lazarus
+--SCRIPTS
+
 -- LOTBS: Legend Of The Bone Sword
--- Divinity Hub: Phantom Forces, Bloxburg, Arsenal, Bad Business, Strucid, Project: Lazarus, Jailbreak, probably forgot a few but i dont give a shit it works on fucking every fps game ever
 -- Robeats: Robeats, Robeats Community Server, basically any game thats similar to robeats
 -- Bark: Lumber Tycoon 2 
 -- SkoopX: Lumber Tycoon 2 
@@ -14,3 +14,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/zopac/lua-stuff/main/
 -- AOT Last Breath Killaura: Attack On Titan: Last Breath
 -- Nullware: all of them its an animations script
 -- NetBypass: the yeah
+
+--HUBS
+
+-- Divinity Hub: Phantom Forces, Bloxburg, Arsenal, Bad Business, Strucid, Project: Lazarus, Jailbreak, probably forgot a few but i dont give a shit it works on fucking every fps game ever
+-- Bruh Hub: Phantom Forces, Shindo Life 2, Vesteria, Ragdoll Engine, Ro-Ghoul, Project: Lazarus
+-- OwlHub: just look at this https://github.com/SiLeNSwOrD/OwlHub/blob/master/Games.txt i cant be bothered to type all of them
