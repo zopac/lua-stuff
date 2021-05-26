b731cc5a3535950d2d972a03a41983f3d937a609
zopac <61770151+zopac@users.noreply.github.com>
2021-05-26 09:52:42 -0500

    brain damage

---
+++ loadstring2.lua
@@ -0,0 +1,25 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zopac/lua-stuff/main/script%20hub%202.lua", true))()
+
+--SCRIPTS
+
+-- Admin: do i need to tell u what the fuck an admin script is
+-- LOTBS: Legend Of The Bone Sword
+-- Robeats: Robeats, Robeats Community Server, basically any game thats similar to robeats
+-- Bark: Lumber Tycoon 2 
+-- SkoopX: Lumber Tycoon 2 
+-- Dungeon Quest Coin Collect: Dungeon Quest
+-- Kix Hub: Your Bizarre Adventure
+-- Rem Hub: Your Bizarre Adventure, King Piece
+-- YBA Gui: Your Bizarre Adventure
+-- Vesteria Pear Sell: Vesteria
+-- AOT Last Breath Killaura: Attack On Titan: Last Breath
+-- Nullware: all of them its an animations script
+-- NetBypass: the yeah
+-- HellReaver: its literally the name its just a script for hellreaver which is basically roblox doom
+-- Notoriety: once again its the fucking name its just a script for notoriety
+
+--HUBS
+
+-- Divinity Hub: Phantom Forces, Bloxburg, Arsenal, Bad Business, Strucid, Project: Lazarus, Jailbreak, probably forgot a few but i dont give a shit it works on fucking every fps game ever
+-- Bruh Hub: Phantom Forces, Shindo Life 2, Vesteria, Ragdoll Engine, Ro-Ghoul, Project: Lazarus
+-- OwlHub: just look at this https://github.com/SiLeNSwOrD/OwlHub/blob/master/Games.txt i cant be bothered to type all of them
